2f891d1f185c05458ecc1fe7fe16491c55242f43
Francisco <codefranc6co@hotmail.com>
2025-07-31 23:55:43 +0100

    test

--- SchoolManagement/testapp/views.py
+++ SchoolManagement/testapp/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def testview(request):
-    return render(request, 'home.html')  # Assure-toi que le fichier s'appelle bien home.html
+    return render(request, 'public/home.html')  # Assure-toi que le fichier s'appelle bien home.html
