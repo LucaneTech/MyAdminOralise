b59852d1ced3c4027f84a3064f8355d04b28fe16
Francisco <codefranc6co@hotmail.com>
2025-08-08 23:53:03 +0100

    some again

--- SchoolManagement/SchoolManagement/settings.py
+++ SchoolManagement/SchoolManagement/settings.py
@@ -189,7 +189,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'SchoolManagement.wsgi.application'
 
 # Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
