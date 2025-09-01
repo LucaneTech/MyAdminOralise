eeeaab84103651c27dd48be40df0a2bea78fa523
Francisco <codefranc6co@hotmail.com>
2025-09-01 17:58:32 +0100

    wsi modifs

--- SchoolManagement/wsgi.py
+++ SchoolManagement/wsgi.py
@@ -1,12 +1,16 @@
-import os
-import sys
+"""
+WSGI config for SchoolManagement project.
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(BASE_DIR)
-sys.path.append(os.path.join(BASE_DIR, 'SchoolManagement'))
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SchoolManagement.SchoolManagement.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SchoolManagement.settings')
 
 application = get_wsgi_application()
