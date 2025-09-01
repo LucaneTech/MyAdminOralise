d3cc3b001a6228e77610fb38006c1f219a75e1af
Francisco <codefranc6co@hotmail.com>
2025-09-01 16:51:39 +0100

    config of start.sh file

--- SchoolManagement/urls.py
+++ SchoolManagement/urls.py
@@ -16,5 +16,8 @@ urlpatterns = [
     path('', include('dashboard.urls')),
 
         
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
