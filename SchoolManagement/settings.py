ebc813ec0409604a6183d48279175ebe9a30826c
Lucane <contact@Lucane.tech>
2026-01-15 12:23:50 +0100

    database_online

--- SchoolManagement/settings.py
+++ SchoolManagement/settings.py
@@ -231,14 +231,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'SchoolManagement.wsgi.application'
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
+
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.sqlite3',
+    #         'NAME': BASE_DIR / 'db.sqlite3',
+    #     }
+    # }
+
     DATABASES = {
         'default': dj_database_url.parse(
             os.environ.get("DATABASE_URL"),
