b746e09afaa2d8e8852cd91a41769e6e17805b08
Lucane <contact@Lucane.tech>
2026-01-15 12:27:41 +0100

    debug of database

--- SchoolManagement/settings.py
+++ SchoolManagement/settings.py
@@ -231,19 +231,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'SchoolManagement.wsgi.application'
 
-
-    # DATABASES = {
-    #     'default': {
-    #         'ENGINE': 'django.db.backends.sqlite3',
-    #         'NAME': BASE_DIR / 'db.sqlite3',
-    #     }
-    # }
-
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
     DATABASES = {
         'default': dj_database_url.parse(
             os.environ.get("DATABASE_URL"),
-            conn_max_age=600,
-            ssl_require=True
         )
     }
 
