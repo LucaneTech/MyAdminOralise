4496e66174cf95c765564b3a4c8a2d5ae59ecee8
Lucane <contact@lucane.tech>
2026-02-14 01:54:22 +0100

    db

--- SchoolManagement/settings.py
+++ SchoolManagement/settings.py
@@ -152,6 +152,8 @@ WSGI_APPLICATION = "SchoolManagement.wsgi.application"
 DATABASES = {
     'default': dj_database_url.parse(
         os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
