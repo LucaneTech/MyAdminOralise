7c434782ebb20277e22c8d86ca2d366a7fabe050
lucane <contact@lucane.tech>
2026-05-25 00:16:17 +0100

    db

--- SchoolManagement/settings.py
+++ SchoolManagement/settings.py
@@ -151,20 +151,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "SchoolManagement.wsgi.application"
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.parse(
-#         os.environ.get("DATABASE_URL"),
-#         conn_max_age=600,
-#         ssl_require=True
-#     )
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 # delete admin django notifications
 MESSAGE_LEVEL = messages.ERROR
